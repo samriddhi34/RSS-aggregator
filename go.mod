e59f1b58cf24cfa982a0426ccba82d49ea139797
Samriddhi Sinha <samriddhisinha52@gmail.com>
2025-03-28 11:11:58 +0530

    Saving changes - chreated a router object and setup for go-chi

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/samriddhi34/RSS-aggregator
 
 go 1.18
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"net/http"
 	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
 )
 
 func main() {
     fmt.Println("Hello, RSS Aggregator!")
-
+	//load environment variable
 	godotenv.Load()
 
+	//get port from environment variable
 	portString := os.Getenv("PORT")
 	if portString == ""{
 		log.Fatal("Port is not Found in the Environment")
 	}
 	fmt.Println("Port :" , portString)
+
+	router := chi.NewRouter() //Creating a router object
+	
+	//initialized the server
+	srv := &http.Server{
+		Handler : router,
+		Addr : ":" + portString,
+	}
+	log.Printf("Server starting on port %v" , portString)
+	err := srv.ListenAndServe()
+	if err!= nil{
+		log.Fatal(err)
+	}
+
 }
